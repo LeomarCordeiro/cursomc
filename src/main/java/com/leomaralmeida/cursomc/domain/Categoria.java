fb1cffac67b9611c2e171b570424ef19c9d396a7
Leomar Cordeiro <leomardanca@gmail.com>
2019-02-09 22:55:23 -0200

    Persistencia Banco Teste

--- src/main/java/com/leomaralmeida/cursomc/domain/Categoria.java
+++ src/main/java/com/leomaralmeida/cursomc/domain/Categoria.java
@@ -1,12 +1,21 @@
 package com.leomaralmeida.cursomc.domain;
 
 import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 //sequencia de bytes
+
+@Entity
 public class Categoria implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome; 
 	
