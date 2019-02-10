c98faa4dc57c3c30b6c69089a3dd324cd208c897
Leomar Cordeiro <leomardanca@gmail.com>
2019-02-10 09:53:02 -0200

    Implementação das camadas de Resource, Repositories, services do domínio Categoria.

---
+++ src/main/java/com/leomaralmeida/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.leomaralmeida.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leomaralmeida.cursomc.domain.Categoria;
+import com.leomaralmeida.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo; 
+	
+	public Categoria buscar (Integer id) {
+		
+		 Optional<Categoria> obj = repo.findById(id); 
+		 return obj.orElse(null); 
+	}
+	
+	
+
+}
